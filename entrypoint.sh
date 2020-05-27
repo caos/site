659c6d9b8afaf3fbd4e93f5a9d37c32b0b615c1e
Max Peintner <max@caos.ch>
2020-05-27 11:58:13 +0200

    fix loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,17 @@
 set -e
 
 ls -la
-
+cd docs/
 t=""
-for dir in */; do
-  t="$t $dir"
+for dir in */
+do
+    dir=${dir%*/}
+    t="$dir $t"
 done
+t=${t%?}
 echo "$t"
 
+cd ..
 npm install
-sh -c "npx sapper export --legacy $*  --entry $t"
+echo "npx sapper export --legacy $* --entry \"$t\""
+sh -c "npx sapper export --legacy $* --entry \"$t\""
